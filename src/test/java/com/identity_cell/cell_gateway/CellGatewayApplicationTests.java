1d7432fda9051b0b561111efced8db73b038a574
S4NT14G0V <santiagot3p@gmail.com>
2025-09-14 12:21:36 -0500

    [FEAT] API Gateway created

---
+++ src/test/java/com/identity_cell/cell_gateway/CellGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.identity_cell.cell_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
